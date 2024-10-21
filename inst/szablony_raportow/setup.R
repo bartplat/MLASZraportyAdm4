0a3fbe42e816ffdb20b06d8c41231f7d21b82a10
bartplat <b.plat@protonmail.com>
2024-10-21 12:01:12 +0200

    Update szablonów raportów do wersji 5-cio statusowej; update pliku setup.R

--- inst/szablony_raportow/setup.R
+++ inst/szablony_raportow/setup.R
@@ -10,13 +10,20 @@ progLiczebnosci = params$progLiczebnosci
 typDokumentu = params$typDokumentu
 wyrownanieTabWykr = params$wyrownanieTabWykr
 rocznik = paste0(params$rok - 1, "/", params$rok)
+zawodowa = ifelse(szk$typ_szk[[1]] %in% c("Branżowa szkoła I stopnia",
+                                          "Branżowa szkoła II stopnia",
+                                          "Technikum",
+                                          "Szkoła policealna"),
+                  1, 0)
 
 # dane
 szk = get(params$obiektWskazniki)
 god = get(params$obiektWskaznikiPorownanie) %>%
   filter(id_szk %in% szk$id_szk)
-szkozaw = szkozaw %>% 
-  filter(id_szk %in% szk$id_szk)
+if (zawodowa == 1) {
+  szkozaw = szkozaw %>% 
+    filter(id_szk %in% szk$id_szk)
+}
 
 # obiekt powiaty
 powiaty_dop = powiaty %>%
